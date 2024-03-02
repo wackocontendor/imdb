a17552b686c8c2e0ef180f9f1932b788036b5157
wackocontendor <65124419+wackocontendor@users.noreply.github.com>
2024-03-02 00:08:21 +0000

    Updated extract and load function

--- dataprep.py
+++ dataprep.py
@@ -6,18 +6,12 @@ import os
 
 url_prefix = "https://datasets.imdbws.com/"
 
-def extract_tsv(url):
+def extract_load_tsv(suffix):
+    url = url_prefix + suffix
     response = requests.get(url)
     gzipped_content = response.content
-    return gzip.decompress(gzipped_content).decode('utf-8')
-
-def create_dataframe(content):
-    return pd.read_csv(io.StringIO(content), sep='\t', dtype=str)
-
-def extract_and_load(suffix):
-    url = url_prefix + suffix
-    tsv_content = extract_tsv(url)
-    return create_dataframe(tsv_content)
+    extracted_tsv = gzip.decompress(gzipped_content).decode('utf-8')
+    return pd.read_csv(io.StringIO(extracted_tsv), sep='\t', dtype=str)
 
 def export_to_csv(df):
     outname = 'test.csv'
@@ -27,12 +21,12 @@ def export_to_csv(df):
     fullname = os.path.join(outdir, outname)    
     return df.to_csv(fullname, index=False)
 
-# title_akas = extract_and_load("title.akas.tsv.gz")
-# title_basics = extract_and_load("title.basics.tsv.gz")
-# title_crew = extract_and_load("title.crew.tsv.gz")
-# title_episodes = extract_and_load("title.episode.tsv.gz")
-# title_principals = extract_and_load("title.principals.tsv.gz")
-title_ratings = extract_and_load("title.ratings.tsv.gz")
-# name_basics = extract_and_load("name.basics.tsv.gz")
+# title_akas = extract_load_tsv("title.akas.tsv.gz")
+# title_basics = extract_load_tsv("title.basics.tsv.gz")
+# title_crew = extract_load_tsv("title.crew.tsv.gz")
+# title_episodes = extract_load_tsv("title.episode.tsv.gz")
+# title_principals = extract_load_tsv("title.principals.tsv.gz")
+title_ratings = extract_load_tsv("title.ratings.tsv.gz")
+# name_basics = extract_load_tsv("name.basics.tsv.gz")
 
 export_to_csv(title_ratings)
